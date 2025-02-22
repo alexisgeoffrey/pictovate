6662ad5764011c507775280aff3d9e3ed78402ad
Alexis Geoffrey <alexisgeoffrey1@gmail.com>
2025-02-22 19:04:47 +0900

    init

---
+++ src/main/java/com/alexisgeoffrey/pictovate/PictovateApplication.java
@@ -0,0 +1,13 @@
+package com.alexisgeoffrey.pictovate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictovateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictovateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexisgeoffrey/pictovate/PictovateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexisgeoffrey.pictovate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictovateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
